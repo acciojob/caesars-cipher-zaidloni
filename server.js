12104ec720d243041ae0062bcee72ea90dabee8a
Darshit Jain <137152681+DarshitJain0704@users.noreply.github.com>
2023-06-30 16:56:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,4 @@
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
-const answer = require("./index.js");
-
+const app = require("./index.js");
 app.listen(3000, () => {
   console.log('server started');
-});
-
-app.get('/', (req, res) => {
-  res.send(answer("SERR YBIR? NPPVBWBO"))
 });
